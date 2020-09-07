a4f8fff54990661bd100acd1f01976169c20a716
joelgraham <joelgraham@joels-macbook-pro.home>
2020-09-07 20:02:13 +0200

    Oppgave 1.3

---
+++ Faculty/src/fa/Factorialz.java
@@ -0,0 +1,18 @@
+package fa;
+
+public class Factorialz {
+
+	public static void main(String[] args) {
+	
+		int number = 5;
+		int y,fact = 1;
+		
+		 for(y=1; y <= number; y++){    
+		      fact=fact*y;    
+		  }    
+		 System.out.println(fact);
+		}
+
+	}
+
+
